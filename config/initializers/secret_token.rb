e187f86002823876265ddc4bc2cb315b16241097
rlau <rlau@mit.edu>
2013-05-12 13:49:11 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reimburse::Application.config.secret_token = 'b0138928a65e7fbd57698a03e93f05149539135ae2591693e0a0dc74bd2fe389e202aa8bf36247220da1577ab08e5ffcf98ca4c20c476213ba1102026f0a2f02'
